d91239d0a3c444525b09e2d6dabd6a1867a605ad
gligor.cimpean <gligor.campean@yahoo.com>
2021-12-02 15:06:50 +0200

    adding web data model. closes #1

---
+++ mssc-beer-service/src/main/java/spring/boot/project/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,4 @@
+package spring.boot.project.msscbeerservice.web.controller;
+
+public class BeerController {
+}
---
+++ mssc-beer-service/src/main/java/spring/boot/project/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package spring.boot.project.msscbeerservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAINSON
+}
